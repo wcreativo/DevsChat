c5116f054612cfaa57211a0374ea097b75da7d43
Wilbert Mojica <wilbertmojica@Wilberts-MacBook-Air.local>
2023-10-15 20:19:13 -0500

    Incorporate User model

--- config/settings.py
+++ config/settings.py
@@ -144,3 +144,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379")
+
+AUTH_USER_MODEL = "users.User"
